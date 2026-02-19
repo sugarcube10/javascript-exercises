4d5af64f7edc597e092db81ce36350d280e4eae8
sugarcube10 <217878983+sugarcube10@users.noreply.github.com>
2026-02-19 19:38:45 +0530

    Complete sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if(num1 >= 0 && num2 >= 0 && Number.isInteger(num1) && Number.isInteger(num2)) {
+
+        let min = Math.min(num1, num2);
+        let max = Math.max(num1, num2);
+        let sum = 0;
+
+        for(let i = min; i <= max; i++) {
+            
+            sum += i;
+
+        }
+        return sum;
+    }
+
+    else return "ERROR";
+
 
 };
 
