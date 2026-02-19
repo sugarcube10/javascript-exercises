1c7754c7e02cb87db0db53006f4a24cc3edb28d2
sugarcube10 <217878983+sugarcube10@users.noreply.github.com>
2026-02-19 20:00:04 +0530

    Complete tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+
+  let temp = Number(((num - 32) * 5 / 9).toFixed(1));
+
+
+  return temp;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+
+  let temp = Number((num * 9 / 5 + 32).toFixed(1));
+  
+
+  return temp;
+
 };
 
 // Do not edit below this line
