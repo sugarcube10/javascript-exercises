ccdd6860a0dc918453840fe1a07fad2dab4bf415
sugarcube10 <217878983+sugarcube10@users.noreply.github.com>
2026-02-20 11:37:51 +0530

    Complete removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+
+    for(let j = 0; j < array.length; j++) {
+
+
+       for(let i = 0; i < num.length; i++) {
+
+
+            let index = array.indexOf(num[i]);
+            if(index == -1) {
+                continue;
+            }
+            array.splice(index, 1);
+
+        }
+    }
+
+    return array
+
 };
 
 // Do not edit below this line
